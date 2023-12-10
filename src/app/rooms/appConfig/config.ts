b07aa052bf6df4c8eed933ca3791ac53f0618294
BrawnyMan <75688115+BrawnyMan@users.noreply.github.com>
2023-12-10 23:42:13 +0100

    Update config.ts

--- src/app/rooms/appConfig/config.ts
+++ src/app/rooms/appConfig/config.ts
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import { environment } from '../../../environments/environment';
 
 export const AppConfig = {
     apiUrl: environment.production
     ? "http://myhost.si"
     : "http://localhost:7392/api/v1.0/room",
-=======
-export const AppConfig = {
-    apiUrl: "http://localhost:7392/api"
->>>>>>> b1f85429dd387ed4cd2ad1752807b698ffce9729
 };
